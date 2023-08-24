474d03204bba41573d7230fe383db1eb540fca37
yoong0304 <yoong0304@naver.com>
2023-08-24 18:17:32 +0900

    0824

--- skycode/src/main/java/teamproject/skycode/review/Review.java
+++ skycode/src/main/java/teamproject/skycode/review/Review.java
@@ -1,10 +1,14 @@
 package teamproject.skycode.review;
 
 
+import java.time.LocalDateTime;
+
 public class Review {
     private Long id;    // 리뷰 id
+    private String reviewTitle; // 리뷰 제목
     private String nickName;    // 작성자 닉네임
     private String body;    // 리뷰 내용
-    private Long regTime;   // 작성시간
+    private String hashTag; // 해시태그
+    private LocalDateTime regTime;   // 작성시간
 
 }
