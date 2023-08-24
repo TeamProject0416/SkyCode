e1483f5589ac221326d060c4617ec2d5974420d5
HoneyJH <wersd0206@naver.com>
2023-08-24 16:33:19 +0900

    0824

--- skycode/src/main/java/teamproject/skycode/mainController/MainController.java
+++ skycode/src/main/java/teamproject/skycode/mainController/MainController.java
@@ -1,13 +1,13 @@
-package teamproject.skycode;
+package teamproject.skycode.mainController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
 //@RequestMapping(value = "/main")
 public class MainController {
-    @GetMapping(value = "/main")
-    public String skyCode() {
-        return "main";
-    }
+
+        @GetMapping(value = "/main")
+        public String skyCode() {
+            return "main";
+        }
 }
