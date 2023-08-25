c23bd5c139d62b87d7fba39d93db33c76448ca00
HoneyJH <wersd0206@naver.com>
2023-08-25 17:26:22 +0900

    0825

--- skycode/src/main/java/teamproject/skycode/myPage/MyPageController.java
+++ skycode/src/main/java/teamproject/skycode/myPage/MyPageController.java
@@ -6,8 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MyPageController {
 
+    @GetMapping(value = "user_shopping/orderList")
+    public String userOrderList(){
+        return "myPage/shopping/orderList";
+    }
+
     @GetMapping(value = "user_shopping/point")
     public String userPoint(){
         return "myPage/shopping/point";
+    }
+
+    @GetMapping(value = "user_shopping/coupon")
+    public String userCoupon(){
+        return "myPage/shopping/coupon";
     }
 }
