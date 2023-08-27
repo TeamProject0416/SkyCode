1d792114355c65f4f3342eaeff127a449eb198d7
HoneyJH <wersd0206@naver.com>
2023-08-27 16:17:01 +0900

    0827

--- skycode/src/main/java/teamproject/skycode/review/Review.java
+++ skycode/src/main/java/teamproject/skycode/review/Review.java
@@ -1,9 +1,13 @@
 package teamproject.skycode.review;
 
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
+@Entity
 public class Review {
+    @Id
     private Long id;    // 리뷰 id
     private String reviewTitle; // 리뷰 제목
     private String nickName;    // 작성자 닉네임
