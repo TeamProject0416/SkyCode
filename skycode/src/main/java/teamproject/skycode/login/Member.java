a5b44fea45ae51b1509e9cd966ba312346008617
wan0706 <wan0706@hanmail.net>
2023-08-28 11:02:57 +0900

    0828

---
+++ skycode/src/main/java/teamproject/skycode/login/Member.java
@@ -0,0 +1,36 @@
+package teamproject.skycode.login;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+@Id
+    private Long id;
+
+    private String password;
+
+    //id 의 getter setter
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // password 의 getter setter
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+}
