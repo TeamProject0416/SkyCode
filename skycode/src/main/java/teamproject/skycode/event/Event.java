1b2f8926349b9a38082247414cfe9a6fe0ab6bab
HoneyJH <wersd0206@naver.com>
2023-08-28 16:39:00 +0900

    0828

--- skycode/src/main/java/teamproject/skycode/event/Event.java
+++ skycode/src/main/java/teamproject/skycode/event/Event.java
@@ -4,12 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
 public class Event {
+    @Id
     private Long id; // 이벤트 Id
+
     private String eventTitle; // 이벤트 제목
     private String nickName; // 작성자 닉네임
     private String content; // 이벤트 내용
