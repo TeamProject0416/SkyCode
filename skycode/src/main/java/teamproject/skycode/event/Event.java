24dd07cac482be7cdae6e2f0fcfedcc060c75b50
moonsunho <tkdlqj0411@naver.com>
2023-08-28 15:25:22 +0900

    0828

--- skycode/src/main/java/teamproject/skycode/event/Event.java
+++ skycode/src/main/java/teamproject/skycode/event/Event.java
@@ -1,11 +1,17 @@
 package teamproject.skycode.event;
 
-import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.Entity;
+import java.time.LocalDateTime;
+@Entity
+@Getter
+@Setter
 public class Event {
-//    private Long id; // 관리자 Id
+    private Long id; // 이벤트 Id
     private String eventTitle; // 이벤트 제목
-//    private String nickName; // 작성자 닉네임
+    private String nickName; // 작성자 닉네임
     private String content; // 이벤트 내용
     private LocalDateTime regTime; // 작성시간
 
