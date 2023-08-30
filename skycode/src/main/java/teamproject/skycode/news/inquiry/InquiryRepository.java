7671e2fa382bb742c09f4f795697abe11870a1c0
naxomis <nayomi@hs.ac.kr>
2023-08-31 00:02:09 +0900

    0831

--- skycode/src/main/java/teamproject/skycode/news/inquiry/InquiryRepository.java
+++ skycode/src/main/java/teamproject/skycode/news/inquiry/InquiryRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 
-public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
+public interface InquiryRepository extends JpaRepository<Inquiry, Long > {
 
 }
