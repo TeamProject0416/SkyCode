816bfa6c284824af8cca11d0f8f3d7289990232d
HoneyJH <wersd0206@naver.com>
2023-08-30 10:27:43 +0900

    0830

--- skycode/src/main/java/teamproject/skycode/review/ReviewController.java
+++ skycode/src/main/java/teamproject/skycode/review/ReviewController.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 
 //@RequestMapping("/review")
 @Controller
-public class R_eviewController {
+public class ReviewController {
 
     @Autowired
     private ReviewRepository reviewRepository;
