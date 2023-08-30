6c4574b094b2e44e577f23e055a9acd17e84ad54
naxomis <nayomi@hs.ac.kr>
2023-08-30 09:10:44 +0900

    0830

--- skycode/src/main/resources/static/js/news/inquiry.js
+++ skycode/src/main/resources/static/js/news/inquiry.js
@@ -11,16 +11,16 @@
 // }
 
 document.addEventListener("DOMContentLoaded", function() {
-    const form = document.getElementsByClassName("inquiry");
-    const inquiryList = document.getElementsByClassName("inquiry-list");
+    const form = document.getElementById("inquiry");
+    const inquiryList = document.getElementById("inquiry-list");
 
     form.addEventListener("submit", function(event) {
         event.preventDefault();
 
         const type = document.getElementById("type").value;
         const isPrivate = document.getElementById("private").checked;
-        const title = document.getElementsByClassName("title").value;
-        const content = document.getElementsByClassName("content").value;
+        const title = document.getElementById("title").value;
+        const content = document.getElementById("content").value;
 
         const listItem = document.createElement("li");
         listItem.innerHTML = `<strong>${type}</strong> - ${title} (${isPrivate ? "나만보기" : "전체공개"})<p>${content}</p>`;
