c63688c576bf9d4f1a25553a610a88e5f2426cb0
HoneyJH <wersd0206@naver.com>
2023-08-30 10:26:40 +0900

    0830

--- skycode/src/main/java/teamproject/skycode/news/inquiry/InquiryController.java
+++ skycode/src/main/java/teamproject/skycode/news/inquiry/InquiryController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class Inquiry_Controller {
+public class InquiryController {
 
     @GetMapping(value = "/news/inquiry")
     public String newsInquiry(){
--- skycode/src/main/java/teamproject/skycode/news/notion/Notion_Controller.java
+++ skycode/src/main/java/teamproject/skycode/news/notion/Notion_Controller.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class NotionController {
+public class Notion_Controller {
 
     @GetMapping(value = "/news/notionUp")
     public String newsNotionUp(){
