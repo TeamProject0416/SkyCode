37f53e1f13b7fd0bc503ece5cf527aa4c8d8b1c0
HoneyJH <wersd0206@naver.com>
2023-08-30 10:12:02 +0900

    0830

--- skycode/src/main/java/teamproject/skycode/news/faq/NewsController.java
+++ skycode/src/main/java/teamproject/skycode/news/faq/NewsController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class FaqController {
+public class NewsController {
 
     @GetMapping(value = "/news/faqUp")
     public String newsFaqUp(){
