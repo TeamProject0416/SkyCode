6719ede4e4a3930714e8442a696d40ba4245874e
yoong0304 <yoong0304@naver.com>
2023-09-04 17:41:01 +0900

    0904

--- skycode/src/main/resources/static/js/sub/footer.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $(document).ready(function() {
-        $(".nyFamily").click(function() {
-            $(".nyFamily ul").toggleClass("opacity");
-            //   console.log(opacity);
-            $(".nyFamilyIcon").toggleClass("rotate");
-        });
-    });
-});
