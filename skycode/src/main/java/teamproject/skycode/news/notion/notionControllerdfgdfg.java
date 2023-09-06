10f2be92737e693d536a2a09c68d29a5b6f12ec2
naxomis <nayomi@hs.ac.kr>
2023-09-06 12:30:10 +0900

    Revert "0906"
    This reverts commit 1ac79314bbe19b6bfbac9cd3179826a5d991830d.

--- skycode/src/main/java/teamproject/skycode/news/notion/notionControllerdfgdfg.java
+++ skycode/src/main/java/teamproject/skycode/news/notion/notionControllerdfgdfg.java
@@ -19,7 +19,7 @@ import java.time.LocalDateTime;
 @RequestMapping("/news")
 @RequiredArgsConstructor
 
-public class NotionController {
+public class notionControllerdfgdfg {
 
     @Autowired
     private NotionRepository notionRepository;
