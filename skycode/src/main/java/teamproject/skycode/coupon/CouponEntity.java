d22030d8f99dd948a0531e6052419dab456d1093
HoneyJH <wersd0206@naver.com>
2023-09-06 10:43:32 +0900

    0906

--- skycode/src/main/java/teamproject/skycode/coupon/CouponEntity.java
+++ skycode/src/main/java/teamproject/skycode/coupon/CouponEntity.java
@@ -1,10 +1,16 @@
 package teamproject.skycode.coupon;
 
 
+import lombok.*;
+
 import javax.persistence.*;
 import javax.validation.constraints.Pattern;
 
+@Getter
 @Entity
+@Table(name = "coupon")
+@Setter
+@ToString
 public class CouponEntity {
     @Id
     @Column(name = "coupon_id")
@@ -25,11 +31,13 @@ public class CouponEntity {
     @Enumerated(EnumType.STRING)
     private CouponStatus couponStatus; // 쿠폰 상태
 
-    private String couponTime; //"yyyy-MM-dd"
-
     private double maxPrice; //최대 할인 금액
 
-    private double useMinPrice; // 최소사용금액
+    private double useMinPrice; // 최소 사용 금액
+
+    private String couponStart; // 쿠폰 시작일 "yyyy-MM-dd"
+
+    private String couponEnd; // 쿠폰 종료일 "yyyy-MM-dd"
 
 
     public Integer getNum() {
