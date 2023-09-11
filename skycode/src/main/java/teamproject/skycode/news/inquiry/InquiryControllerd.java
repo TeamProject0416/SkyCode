e2eadf8de9bd85e360202da41976b787b69f4b70
HoneyJH <wersd0206@naver.com>
2023-09-11 09:37:16 +0900

    0911

--- skycode/src/main/java/teamproject/skycode/news/inquiry/InquiryControllerd.java
+++ skycode/src/main/java/teamproject/skycode/news/inquiry/InquiryControllerd.java
@@ -1,20 +1,15 @@
 package teamproject.skycode.news.inquiry;
 
-import lombok.RequiredArgsConstructor;
-import lombok.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/news")
-public class inquiryController {
+public class InquiryControllerd {
 
 
     @Autowired
