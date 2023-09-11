6c77e861e0e4c8555ed16edd78bc4006a1210640
HoneyJH <wersd0206@naver.com>
2023-09-11 09:38:26 +0900

    0911

--- skycode/src/main/java/teamproject/skycode/common/BaseEntity.java
+++ skycode/src/main/java/teamproject/skycode/common/BaseEntity.java
@@ -2,16 +2,12 @@ package teamproject.skycode.common;
 
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import teamproject.skycode.login.BaseTimeEntity;
 
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
 
 @EntityListeners(value = {AuditingEntityListener.class})
 @MappedSuperclass
