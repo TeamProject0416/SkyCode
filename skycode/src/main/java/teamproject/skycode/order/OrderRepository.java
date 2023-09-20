60088497f8a10c71609739b44ef9482e1faae352
HoneyJH <wersd0206@naver.com>
2023-09-20 14:37:17 +0900

    0920

--- skycode/src/main/java/teamproject/skycode/order/OrderRepository.java
+++ skycode/src/main/java/teamproject/skycode/order/OrderRepository.java
@@ -1,11 +1,14 @@
 package teamproject.skycode.order;
 
+import com.querydsl.core.types.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import teamproject.skycode.constant.OrderStatus;
 import teamproject.skycode.login.MemberEntity;
+
+import java.util.List;
 
 
 @Repository
@@ -32,4 +35,6 @@ public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
     );
 
     OrderEntity findByTotalPriceAndOrderStatusAndMemberEntity(int paymentAmount, OrderStatus orderStatus, MemberEntity memberEntity);
+
+    List<OrderEntity> findByMemberEntityId(Long id);
 }
